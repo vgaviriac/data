bbfa85d65c125fe3936a3940ded598ae80d3c04b
vgaviriac <vgaviriac1@eafit.edu.co>
2024-05-27 16:28:47 -0500

    Update humedad.py

--- pages/humedad.py
+++ pages/humedad.py
@@ -1,3 +1,40 @@
 import streamlit as st
+import pandas as pd
+from PIL import Image
 
 st.title ('Analisis de humedad')
+image = Image.open('grafana2.jpg')
+st.image(image)
+
+uploaded_file = st.file_uploader('Choose a file')
+
+if uploaded_file is not None:
+   df1=pd.read_csv(uploaded_file)
+
+   st.subheader('Perfil gráfico de la variable medida.')
+   df1 = df1.set_index('Time')
+   st.line_chart(df1)
+   
+   st.write(df1)
+   st.subheader('Estadísticos básicos de los sensores.')
+   st.dataframe(df1["Humedad ESP32"].describe())
+   
+   min_temp = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
+   # Filtrar el DataFrame utilizando query
+   filtrado_df_min = df1.query(f"`Humedad ESP32` > {min_temp}")
+   # Mostrar el DataFrame filtrado
+   st.subheader("Temperaturas superiores al valor configurado.")
+   st.write('Dataframe Filtrado')
+   st.write(filtrado_df_min)
+   
+   max_temp = st.slider('Selecciona valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2)
+   # Filtrar el DataFrame utilizando query
+   filtrado_df_max = df1.query(f"`Humedazd ESP32` < {max_temp}")
+   # Mostrar el DataFrame filtrado
+   st.subheader("Temperaturas Inferiores al valor configurado.")
+   st.write('Dataframe Filtrado')
+   st.write(filtrado_df_max)
+   
+
+else:
+ st.warning('Necesitas cargar un archivo csv excel.')
