b34a002126b9c87c679163fdb4bdd9e0bcc36cca
vgaviriac <vgaviriac1@eafit.edu.co>
2024-05-29 18:03:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 
 st.title('Análisis de datos de Sensores en Mi Ciudad')
-image = Image.open('grafana2.jpg')
+image = Image.open('temperatura.png')
 st.image(image)
 
 uploaded_file = st.file_uploader('Choose a file')
